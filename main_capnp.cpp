06c191e811238e2c73496e84af128d4b1faeda7c
Pavel Davydov <pdavydov108@gmail.com>
2021-11-04 16:46:43 +0300

    Add more benchmarks.

--- main_capnp.cpp
+++ main_capnp.cpp
@@ -1,3 +1,32 @@
-int main() {
-    return 0;
+#include <benchmark/benchmark.h>
+#include <iostream>
+#include <messages/capnproto/login.capnp.h>
+#include <messages/capnproto/transaction.capnp.h>
+#include <capnp/message.h>
+#include <capnp/serialize-packed.h>
+#include <capnp/serialize.h>
+
+static void BM_login(benchmark::State &state) {
+    while (state.KeepRunning()) {
+        capnp::MallocMessageBuilder message;
+        auto login = message.initRoot<Login>();
+        login.setHostname("localhost");
+        login.setName("pablo");
+        login.setObjectType(1);
+        login.setPassword("secret");
+        login.setTimeout(1000);
+        auto arr = capnp::messageToFlatArray(message);
+        std::cout << arr.size() << std::endl;
+    }
 }
+BENCHMARK(BM_login);
+
+static void BM_transaction(benchmark::State &state) {
+    while (state.KeepRunning()) {
+        capnp::MallocMessageBuilder message;
+        auto login = message.initRoot<Transaction>();
+        login.setPriority(1);
+        auto arr = capnp::messageToFlatArray(message);
+    }
+}
+BENCHMARK(BM_transaction);
---
+++ messages/capnproto/transaction.capnp
@@ -0,0 +1,5 @@
+@0xb27a9a988750a7cc;
+
+struct Transaction {
+    priority @0: UInt8;
+}
