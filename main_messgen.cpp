3dbc2f3acb98538f042f9c689eb2b0b7d7589dcc
Pavel Davydov <pdavydov108@gmail.com>
2022-06-28 17:32:12 +0300

    Fix build for messgen.

--- main_messgen.cpp
+++ main_messgen.cpp
@@ -1,12 +1,12 @@
 #include <benchmark/benchmark.h>
 #include <messgen/Metadata.h>
-#include <messages/msgs/messgen/messages.h>
+#include <messages/msgs/protocol/test/messages.h>
 #include <vector>
 
 static void BM_login(benchmark::State &state) {
     std::vector<char> buffer(1024, 0);
     while (state.KeepRunning()) {
-        messages::msgs::messgen::login_flat lf;
+        messages::msgs::protocol::test::login_flat lf;
         lf.hostname = "localhost";
         lf.object_name = "pablo";
         lf.object_type = 1;
@@ -23,14 +23,14 @@ static void BM_login_parse(benchmark::State &state) {
     std::string name;
     messgen::MemoryAllocator alloc{reinterpret_cast<uint8_t*>(mem.data()), mem.size()};
     while (state.KeepRunning()) {
-        messages::msgs::messgen::login_flat lf;
+        messages::msgs::protocol::test::login_flat lf;
         lf.hostname = "localhost";
         lf.object_name = name;
         lf.object_type = 1;
         lf.timeout = 1024;
         lf.password = "secret";
         auto size = lf.serialize_msg(reinterpret_cast<uint8_t*>(buffer.data()));
-        messages::msgs::messgen::login_flat other;
+        messages::msgs::protocol::test::login_flat other;
         other.parse_msg(reinterpret_cast<uint8_t*>(buffer.data()), size, alloc);
     }
 }
@@ -39,7 +39,7 @@ BENCHMARK(BM_login_parse);
 static void BM_transaction(benchmark::State &state) {
     std::vector<char> buffer(1024, 0);
     while (state.KeepRunning()) {
-        messages::msgs::messgen::transaction tr;
+        messages::msgs::protocol::test::transaction tr;
         tr.prio = 1;
         benchmark::DoNotOptimize(tr.serialize_msg(reinterpret_cast<uint8_t*>(buffer.data())));
     }
